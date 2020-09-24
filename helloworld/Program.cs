2543db4e73189f23f00db5ad3475b53d210e7368
Anuj Sharma <intrstelr.007@gmail.com>
2020-09-24 10:23:07 +0530

    [Anuj] Add. helloworld

---
+++ helloworld/Program.cs
@@ -0,0 +1,22 @@
+﻿using helloworld.Services;
+using System;
+//using helloworld.Services;
+
+namespace helloworld
+{
+    class Program
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            addition cal = new addition();
+            int c = cal.AddNumber(4, 5);
+
+            //Console.WriteLine("Hello World!");
+            //string xyz = Console.ReadLine();
+            Console.WriteLine(c);
+
+        }
+    }
+}
---
+++ helloworld/Services/addition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helloworld.Services
+{
+    public class addition
+    {
+        public int AddNumber(int a , int b)
+        {
+            return a + b;
+        }
+    }
+}
